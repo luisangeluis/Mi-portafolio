cab530ac63483da9e8beb1be9944958f9322f2ea
luisangeluis <luisangeluis@gmail.com>
2021-09-10 13:56:31 -0500

    Se está trabajando en las animaciones del menu
    Se esta trabajando para que se agregue la clase active a los elementos
    del menu principal al hacer scroll

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const menuPrincipal = document.querySelector('#menu-principal_ul');
 const optionsMainList = Array.from(menuPrincipal.children);
 
+//Pintar los botones del menu principal al hacer click
 menuPrincipal.addEventListener('click', (e) => {
     let elemento = e.target;
     
@@ -11,4 +12,23 @@ menuPrincipal.addEventListener('click', (e) => {
     
 
 })
+const SectionsGroup = document.querySelector('.scrollspy-example');
+
+addEventListener('scroll',(e)=>{
+
+    let seccionesPrincipales = [...SectionsGroup.children]
+    console.log(seccionesPrincipales);
+
+    for(i=0;i<seccionesPrincipales.length;i++){
+        if(seccionesPrincipales[i].scrollTop===0){
+            console.log(seccionesPrincipales[i].id);
+
+            console.log(seccionesPrincipales[i].scrollTop);
+        }
+    }
+        
+    
+})
+
+
 
