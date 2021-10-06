5f872fa3aac7f379bf4a0f8ddab3080e735e14c3
luisangeluis <luisangeluis@gmail.com>
2021-10-06 13:22:32 -0500

    Se usa intersection observer para animacion pagina
    Se usa intersection observer para que las animaciones funciones con
    responsive page, hacer mas pruebas y aplicarlo a la otra u otras
    animaciones.

--- js/index.js
+++ js/index.js
@@ -43,48 +43,81 @@ addEventListener('scroll',()=>{
     }
 })
 
-//Efecto suave hacia arriba de los elementos 
-const elementosAnimados = document.querySelectorAll('.animado');
-addEventListener('scroll', () => {
-    scrollTopPagina = document.documentElement.scrollTop;
 
-    // console.log(scrollTopPagina);
-    // console.log(typeof elementosAnimados);
+//Opciones del observador
+options={
+    //root:
+    //rootMargin y threshold nos ayudan a indicar cuando quiero que se dispare el callback
+    rootMargin: '-80px'
+    //threshold = Se lanza el callback cuando el elemento se alcanza a ver al 25% y cuando se está ocultando y queda 25% en este ejemplo
+    // threshold:0.25
+}
+//Callback del observador
+const animadoArriba =(entryes)=>{
+    entryes.forEach(entry=>{
+        // entry.target.classList.remove('mostrarArriba');
 
-    arrayAnimados = Array.from(elementosAnimados);
+        if(entry.isIntersecting){
 
-    elementosAnimados.forEach((element) => {
-        // console.log(element);
-
-        if (scrollTopPagina > element.offsetTop - 1000) {
-            // element.style.opacity = '1';
-            element.classList.add('mostrarArriba');
-        }
-
-        if(scrollTopPagina < element.offsetTop-500)
-        {
-            element.classList.remove('mostrarArriba');
-
-        }
+            entry.target.classList.add('mostrarArriba')
+        }  
     })
+}
+//Efecto suave hacia arriba de los elementos 
+const elementosAnimadoArriba = document.querySelectorAll('.animado');
+//Se crea el observador
+const observerAnimadoArriba = new IntersectionObserver(animadoArriba,options);
+
+elementosAnimadoArriba.forEach(element=>{
+    observerAnimadoArriba.observe(element);
 })
+
+
+
+
+addEventListener('scroll', () => {
+    // scrollTopPagina = document.documentElement.scrollTop;
+    // //console.log(scrollTopPagina);
+    // // console.log(typeof elementosAnimados);
+    // arrayAnimados = Array.from(elementosAnimados);
+
+    // elementosAnimados.forEach((element) => {
+    //     // console.log(element);
+
+    //     if (scrollTopPagina > element.offsetTop - 1000) {
+    //         // element.style.opacity = '1';
+    //         element.classList.add('mostrarArriba');
+    //     }
+
+    //     if(scrollTopPagina < element.offsetTop-500)
+    //     {
+    //         element.classList.remove('mostrarArriba');
+
+    //     }
+    // })
+})
+
+
 
 const animadosDos = document.querySelectorAll('.animado-2')
 addEventListener('scroll',()=>{
     const ArrayAnimadoDos = Array.from(animadosDos);
     let scrollTopPagina = document.documentElement.scrollTop;
     
-    ArrayAnimadoDos.forEach( element =>{
+    animadosDos.forEach( element =>{
         if(scrollTopPagina > element.offsetTop -500){
             element.classList.add('mostrar-lado');
         }
         if(scrollTopPagina < element.offsetTop-500)
         {
+            
             element.classList.remove('mostrar-lado');
 
         }
         
     })
+
+    console.log(screen.orientation);
 })
 
 
