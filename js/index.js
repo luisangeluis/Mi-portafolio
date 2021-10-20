eee455467bf5f0f67470868e4dbe208b9fd099f4
luisangeluis <luisangeluis@gmail.com>
2021-10-20 13:38:44 -0500

    Revisar como implementar la class active en observer
    Se esta agregando la clase active con observer en lugar de con evento
    scroll.

--- js/index.js
+++ js/index.js
@@ -23,102 +23,92 @@ menuPrincipal.addEventListener('click', (e) => {
 
 //Agregar clase active a los elementos del menu principal en scroll
 const mainMenuOptions = document.querySelector('#menu-principal_ul');
-arrayOptions = Array.from(mainMenuOptions.children);
+// arrayOptions = Array.from(mainMenuOptions.children);
+arrayOptions = mainMenuOptions.children;
+
 
 const mainSections = document.querySelector('.scrollspy-example');
 const arraySections = Array.from(mainSections.children);
+
 console.log(arraySections);
 // console.log(arrayOptions);
 
-addEventListener('scroll',()=>{
-    // arrayOptions[1].lastElementChild.classList.add('active');
-    scrollTopPagina = document.documentElement.scrollTop;
+// addEventListener('scroll',()=>{
+//     // arrayOptions[1].lastElementChild.classList.add('active');
+//     scrollTopPagina = document.documentElement.scrollTop;
 
-    for(let i=0; i<arraySections.length;i++){
-        let topSection = arraySections[i].offsetTop;
-        if(scrollTopPagina>topSection-300){
-            arrayOptions.forEach(element=>element.firstElementChild.classList.remove('active'))
-            arrayOptions[i].firstElementChild.classList.add('active');
-        }
-    }
-})
+//     for(let i=0; i<arraySections.length;i++){
+//         let topSection = arraySections[i].offsetTop;
+//         if(scrollTopPagina>topSection-300){
+//             arrayOptions.forEach(element=>element.firstElementChild.classList.remove('active'))
+//             arrayOptions[i].firstElementChild.classList.add('active');
+//         }
+//     }
+// })
 
 
 //Opciones del observador
 options={
     //root:
     //rootMargin y threshold nos ayudan a indicar cuando quiero que se dispare el callback
-    rootMargin: '-80px'
+    rootMargin: '0px'
     //threshold = Se lanza el callback cuando el elemento se alcanza a ver al 25% y cuando se está ocultando y queda 25% en este ejemplo
     // threshold:0.25
 }
 //Callback del observador
 const animadoArriba =(entryes)=>{
+    // entryes.forEach(entry=>{
+    //     // entry.target.classList.remove('mostrarArriba');
+
+    //     if(entry.isIntersecting){
+
+    //         entry.target.classList.add('mostrarArriba')
+    //     }  
+    // })
+    
+
+    // for(let entry of entryes){
+    //     console.log(entry);
+    //     if(entry.isIntersecting){
+    //         entry.target.classList.add('mostrarArriba');
+    //     }
+    // }
+    for(let i =0; i<entryes.length;i++){
+        if(entryes[i].isIntersecting){
+            entryes[i].target.classList.add('mostrarArriba');
+            // arrayOptions[i].children[0].classList.add('active');
+            console.log(i)
+
+        }
+    }
+    
+}
+
+const animadoDerecha = (entryes)=>{
     entryes.forEach(entry=>{
-        // entry.target.classList.remove('mostrarArriba');
-
         if(entry.isIntersecting){
-
-            entry.target.classList.add('mostrarArriba')
-        }  
+            
+            entry.target.classList.add('mostrar-lado');
+        }
     })
 }
 //Efecto suave hacia arriba de los elementos 
 const elementosAnimadoArriba = document.querySelectorAll('.animado');
-//Se crea el observador
+//Efecto suave hacia la derecha de los elementos 
+const itemsAnimadosDerecha = document.querySelectorAll('.animado-2')
+//Se crea el observador para los elementos que se animan hacia arriba
 const observerAnimadoArriba = new IntersectionObserver(animadoArriba,options);
+const observerAnimadoDerecha = new IntersectionObserver(animadoDerecha,options);
 
 elementosAnimadoArriba.forEach(element=>{
     observerAnimadoArriba.observe(element);
 })
 
-
-
-
-addEventListener('scroll', () => {
-    // scrollTopPagina = document.documentElement.scrollTop;
-    // //console.log(scrollTopPagina);
-    // // console.log(typeof elementosAnimados);
-    // arrayAnimados = Array.from(elementosAnimados);
-
-    // elementosAnimados.forEach((element) => {
-    //     // console.log(element);
-
-    //     if (scrollTopPagina > element.offsetTop - 1000) {
-    //         // element.style.opacity = '1';
-    //         element.classList.add('mostrarArriba');
-    //     }
-
-    //     if(scrollTopPagina < element.offsetTop-500)
-    //     {
-    //         element.classList.remove('mostrarArriba');
-
-    //     }
-    // })
+itemsAnimadosDerecha.forEach(element=>{
+    observerAnimadoDerecha.observe(element);
 })
 
 
-
-const animadosDos = document.querySelectorAll('.animado-2')
-addEventListener('scroll',()=>{
-    const ArrayAnimadoDos = Array.from(animadosDos);
-    let scrollTopPagina = document.documentElement.scrollTop;
-    
-    animadosDos.forEach( element =>{
-        if(scrollTopPagina > element.offsetTop -500){
-            element.classList.add('mostrar-lado');
-        }
-        if(scrollTopPagina < element.offsetTop-500)
-        {
-            
-            element.classList.remove('mostrar-lado');
-
-        }
-        
-    })
-
-    console.log(screen.orientation);
-})
 
 
 
