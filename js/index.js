dfdff292e9b1b0bb57cfb8e0b8e72a6ad9a40740
luisangeluis <luisangeluis@gmail.com>
2021-12-08 16:35:10 -0600

    Active al menu principal size normal y responsive
    Ya se agrega active al tamaño normal y en responsive y ya hay efectos
    correctamente en los 2 tamaños

--- js/index.js
+++ js/index.js
@@ -1,21 +1,3 @@
-const menuPrincipal = document.querySelector('#menu-principal_ul');
-const optionsMainList = Array.from(menuPrincipal.children);
-
-//Pintar los botones del menu principal al hacer click
-menuPrincipal.addEventListener('click', (e) => {
-    // console.log(e.target);
-    if (e.target && e.target.tagName === 'A') {
-        let elemento = e.target;
-
-        for (let i = 0; i < optionsMainList.length; i++) {
-            if (optionsMainList[i].firstElementChild.classList.contains('active')) {
-                optionsMainList[i].firstElementChild.classList.remove('active');
-            }
-        }
-
-        elemento.classList.add('active');
-    }
-})
 
 //Agregar clase active a los elementos del menu principal en scroll
 const mainMenuOptions = document.querySelector('#menu-principal_ul');
@@ -28,67 +10,38 @@ const arraySections = Array.from(mainSections.children);
 options = {
     //root:
     //rootMargin y threshold nos ayudan a indicar cuando quiero que se dispare el callback
-    // rootMargin: '25%'
+    rootMargin: '25%'
     //threshold = Se lanza el callback cuando el elemento se alcanza a ver al 25% y cuando se está ocultando y queda 25% en este ejemplo
-    threshold: 0.70
+    // threshold: 0.70
 }
 //Callback del observador
-const animadoArriba = (entryes) => {
-
-    for (let x = 0; x < entryes.length; x++) {
-        if (entryes[x].isIntersecting) {
-
-            //aqui se obtiene la clase de las secciones
-            console.log(entryes[x].target.classList[0]);
-
-            arrayOptions.forEach(element => {
-                //se obtiene la clase de cada link
-                console.log(element.firstElementChild.textContent.toLowerCase())
-
-                if (entryes[x].target.classList[0] == element.firstElementChild.textContent.toLowerCase()) {
-                    console.log('hola');
-                    for (let i = 0; i < arrayOptions.length; i++) {
-                        if(arrayOptions[i].firstElementChild.classList.contains('active'))
-                            arrayOptions[i].firstElementChild.classList.remove('active');
-                    }
-                    element.firstElementChild.classList.add('active');
-                }
-            })
-
-
-            console.log(entryes[x]);
-            entryes[x].target.classList.add('mostrarArriba');
-        }
-        // console.log(entryes[x]);
-    }
-
-}
-
 const animadoDerecha = (entryes) => {
     entryes.forEach(entry => {
         if (entry.isIntersecting) {
 
             entry.target.classList.add('mostrar-lado');
+
+            for(let i=0; i<arrayOptions.length;i++){
+                if(entry.target.classList[0] == arrayOptions[i].firstElementChild.textContent.toLowerCase()){
+
+                    arrayOptions[i].firstElementChild.classList.add('active');
+                }else{
+                    arrayOptions[i].firstElementChild.classList.remove('active');
+
+                }
+            }
         }
     })
 }
-//Efecto suave hacia arriba de los elementos al hacer scroll
-const elementosAnimadoArriba = document.querySelectorAll('.animado');
 //Efecto suave hacia la derecha de los elementos al hacer scroll
 const itemsAnimadosDerecha = document.querySelectorAll('.animado-2')
 //Se crea el observador para los elementos que se animan hacia arriba
 const observerAnimadoDerecha = new IntersectionObserver(animadoDerecha, options);
-const observerAnimadoArriba = new IntersectionObserver(animadoArriba, options);
 
 itemsAnimadosDerecha.forEach(element => {
     observerAnimadoDerecha.observe(element);
 })
 
-for (let i = 0; i < elementosAnimadoArriba.length; i++) {
-
-    observerAnimadoArriba.observe(elementosAnimadoArriba[i]);
-
-}
 
 
 
