8ca0fb7264462c728c8a82bfbef8b12d2cde7987
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2022-03-23 16:04:48 -0600

    animaciones y clase active en menu principal
    Se agrega animaciones en todo momento y  ya se agrega la clase active correctamente.
    Jugar con mas animaciones.

--- js/index.js
+++ js/index.js
@@ -1,86 +1,51 @@
-
 //Agregar clase active a los elementos del menu principal en scroll
 const mainMenuOptions = document.querySelector('#menu-principal_ul');
 arrayOptions = Array.from(mainMenuOptions.children);
 const mainSections = document.querySelector('.scrollspy-example');
 const arraySections = Array.from(mainSections.children);
 
-// mainMenuOptions.addEventListener('click',(e)=>{
-//     e.preventDefault();
-//     let event = e
-
-//     arrayOptions.forEach(element=>{
-//         // if(element.firstElementChild.classList.contains('active')){
-//         //     element.firstElementChild.classList.remove('active');
-
-//         // }
-//         if(element !==event){
-//             element.firstElementChild.classList.remove('active');
-
-//         }
-//     })
-
-//     if(event.target && event.target.tagName==='A'){
-//         event.target.classList.add('active');
-//     }
-
-    
-    
-// })
-
 //Efecto suave hacia arriba y efecto suave hacia la derecha de los elementos al hacer scroll
 //Opciones del observador
 options = {
-    root:null,
-    //rootMargin y threshold nos ayudan a indicar cuando quiero que se dispare el callback
-    // rootMargin: '-35% 0%',  
-    rootMargin: '-50% 0%',  
+        root: null,
+        //rootMargin y threshold nos ayudan a indicar cuando quiero que se dispare el callback
+        // rootMargin: '-35% 0%',  
+        rootMargin: '-50% 0%',
 
 
-    //threshold = Se lanza el callback cuando el elemento se alcanza a ver al 25% y cuando se está ocultando y queda 25% en este ejemplo
-    // threshold: 0.3
-}
-//Callback del observador
+        //threshold = Se lanza el callback cuando el elemento se alcanza a ver al 25% y cuando se está ocultando y queda 25% en este ejemplo
+        // threshold: 0.3
+    }
+    //Callback del observador
 const animadoDerecha = (entryes) => {
-     entryes.forEach((entry,i) => {
-        if (entry.isIntersecting) {
-            // console.log(entry)
-            entry.target.classList.add('mostrar-lado');
 
-            // console.log(`parte intersectada ${entry.target.classList[0]}`);
+        entryes.forEach((entry) => {
+            if (entry.isIntersecting) {
+                // console.log(entry)
+                entry.target.classList.add('mostrar-lado');
 
-            for(let i=0; i<arrayOptions.length;i++){
-                // console.log(arrayOptions[i].firstElementChild.href);
-                // if(entry.target.classList[0] == arrayOptions[i].firstElementChild.textContent.toLowerCase()){
-                if(entry.target.classList[0] == arrayOptions[i].firstElementChild.name){
+                for (let i = 0; i < arrayOptions.length; i++) {
 
-                    arrayOptions[i].firstElementChild.classList.add('active');
-                }else{
-                    arrayOptions[i].firstElementChild.classList.remove('active');
+                    if (entry.target.parentNode.classList[0] == arrayOptions[i].firstElementChild.name) {
 
+                        arrayOptions[i].firstElementChild.classList.add('active');
+                    } else {
+                        arrayOptions[i].firstElementChild.classList.remove('active');
+
+                    }
                 }
+            } else {
+                entry.target.classList.remove('mostrar-lado');
+
             }
-            
-        }
-       
-        
-    })
-    
-}
-//Efecto suave hacia la derecha de los elementos al hacer scroll
+        })
+
+    }
+    //Efecto suave hacia la derecha de los elementos al hacer scroll
 const itemsAnimadosDerecha = document.querySelectorAll('.animado-2')
-//Se crea el observador para los elementos que se animan hacia arriba
-const observerAnimadoDerecha = new IntersectionObserver(animadoDerecha,options);
+    //Se crea el observador para los elementos que se animan hacia arriba
+const observerAnimadoDerecha = new IntersectionObserver(animadoDerecha, options);
 
 itemsAnimadosDerecha.forEach(element => {
     observerAnimadoDerecha.observe(element);
 })
-
-
-
-
-
-
-
-
-
