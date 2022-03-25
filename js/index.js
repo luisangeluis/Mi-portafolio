0ce326f3c38a364b0adb5ead90b5a6064c1058c0
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2022-03-25 17:36:23 -0600

    Animaciones.
    Hacer pruebas para ver si funcionan las animaciones de manera responsive.

--- js/index.js
+++ js/index.js
@@ -7,10 +7,21 @@ const arraySections = Array.from(mainSections.children);
 //Efecto suave hacia arriba y efecto suave hacia la derecha de los elementos al hacer scroll
 //Opciones del observador
 options = {
-        root: null,
+    root: null,
+    //rootMargin y threshold nos ayudan a indicar cuando quiero que se dispare el callback
+    // rootMargin: '-35% 0%',  
+    rootMargin: '-50% 0%',
+
+
+    //threshold = Se lanza el callback cuando el elemento se alcanza a ver al 25% y cuando se está ocultando y queda 25% en este ejemplo
+    // threshold: 0.3
+}
+
+options2 = {
+        // root: null,
         //rootMargin y threshold nos ayudan a indicar cuando quiero que se dispare el callback
         // rootMargin: '-35% 0%',  
-        rootMargin: '-50% 0%',
+        // rootMargin: '-50% 0%',
 
 
         //threshold = Se lanza el callback cuando el elemento se alcanza a ver al 25% y cuando se está ocultando y queda 25% en este ejemplo
@@ -19,33 +30,54 @@ options = {
     //Callback del observador
 const animadoDerecha = (entryes) => {
 
-        entryes.forEach((entry) => {
-            if (entry.isIntersecting) {
-                // console.log(entry)
-                entry.target.classList.add('mostrar-lado');
+    entryes.forEach((entry) => {
+        if (entry.isIntersecting) {
+            // console.log(entry)
+            entry.target.classList.add('mostrar-lado');
 
-                for (let i = 0; i < arrayOptions.length; i++) {
+            for (let i = 0; i < arrayOptions.length; i++) {
 
-                    if (entry.target.parentNode.classList[0] == arrayOptions[i].firstElementChild.name) {
+                if (entry.target.parentNode.classList[0] == arrayOptions[i].firstElementChild.name) {
 
-                        arrayOptions[i].firstElementChild.classList.add('active');
-                    } else {
-                        arrayOptions[i].firstElementChild.classList.remove('active');
+                    arrayOptions[i].firstElementChild.classList.add('active');
+                } else {
+                    arrayOptions[i].firstElementChild.classList.remove('active');
 
-                    }
                 }
+            }
+        } else {
+            entry.target.classList.remove('mostrar-lado');
+
+        }
+    })
+
+}
+
+const animadoIzquierda = (entryes) => {
+        // console.log(entryes);
+
+        entryes.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('mostrar-izquierda');
+
             } else {
-                entry.target.classList.remove('mostrar-lado');
+                console.log('no es observado');
+                entry.target.classList.remove('mostrar-izquierda');
 
             }
-        })
-
+        });
     }
     //Efecto suave hacia la derecha de los elementos al hacer scroll
 const itemsAnimadosDerecha = document.querySelectorAll('.animado-2')
-    //Se crea el observador para los elementos que se animan hacia arriba
-const observerAnimadoDerecha = new IntersectionObserver(animadoDerecha, options);
-
+const itemsAnimadosIzquierda = document.querySelectorAll('.animado-3');
+//Se crea el observador para los elementos que se animan hacia arriba
+const observerAnimadoDerecha = new IntersectionObserver(animadoDerecha);
+const observerAnimadoIzquierda = new IntersectionObserver(animadoIzquierda);
+//Se empiezan a observar.
 itemsAnimadosDerecha.forEach(element => {
     observerAnimadoDerecha.observe(element);
 })
+
+itemsAnimadosIzquierda.forEach(element => {
+    observerAnimadoIzquierda.observe(element);
+});
