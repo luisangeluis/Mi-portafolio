e0a91861a5ed748c9e74fa253e07b2dd5729654d
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2022-03-30 15:20:54 -0600

    Animaciones
    Se separa las animaciones del menú principal, de las demas.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 //Agregar clase active a los elementos del menu principal en scroll
 const mainMenuOptions = document.querySelector('#menu-principal_ul');
 arrayOptions = Array.from(mainMenuOptions.children);
-const mainSections = document.querySelector('.scrollspy-example');
-const arraySections = Array.from(mainSections.children);
 
-//Efecto suave hacia arriba y efecto suave hacia la derecha de los elementos al hacer scroll
+
+//CONFIGURACION DEL OBSERVER
 //Opciones del observador
 options = {
     root: null,
@@ -18,16 +17,46 @@ options = {
 }
 
 options2 = {
-        // root: null,
-        //rootMargin y threshold nos ayudan a indicar cuando quiero que se dispare el callback
-        // rootMargin: '-35% 0%',  
-        // rootMargin: '-50% 0%',
+    // root: null,
+    //rootMargin y threshold nos ayudan a indicar cuando quiero que se dispare el callback
+    // rootMargin: '-35% 0%',  
+    // rootMargin: '-50% 0%',
 
 
-        //threshold = Se lanza el callback cuando el elemento se alcanza a ver al 25% y cuando se está ocultando y queda 25% en este ejemplo
-        // threshold: 0.3
-    }
-    //Callback del observador
+    //threshold = Se lanza el callback cuando el elemento se alcanza a ver al 25% y cuando se está ocultando y queda 25% en este ejemplo
+    // threshold: 0.3
+}
+
+//CALLBACKS DEL OBSERVADOR
+
+const callbackSections = entryes => {
+    entryes.forEach(entry => {
+
+        // console.log(element);
+        if (entry.isIntersecting) {
+
+            let nombreDelElemento = entry.target.id;
+            // console.log(nombreDelElemento);
+
+            // const option = arrayOptions.find(element => element.firstElementChild.name == nombreDelElemento);
+            // console.log(option);
+
+            arrayOptions.forEach(option => {
+                console.log(option.firstElementChild.name);
+                if (option.firstElementChild.name == nombreDelElemento) {
+                    option.firstElementChild.classList.add('active');
+
+                } else {
+                    option.firstElementChild.classList.remove('active');
+
+                }
+            })
+
+
+
+        }
+    })
+}
 const animadoDerecha = (entryes) => {
 
     entryes.forEach((entry) => {
@@ -35,16 +64,16 @@ const animadoDerecha = (entryes) => {
             // console.log(entry)
             entry.target.classList.add('mostrar-lado');
 
-            for (let i = 0; i < arrayOptions.length; i++) {
+            // for (let i = 0; i < arrayOptions.length; i++) {
 
-                if (entry.target.parentNode.classList[0] == arrayOptions[i].firstElementChild.name) {
+            //     if (entry.target.parentNode.classList[0] == arrayOptions[i].firstElementChild.name) {
 
-                    arrayOptions[i].firstElementChild.classList.add('active');
-                } else {
-                    arrayOptions[i].firstElementChild.classList.remove('active');
+            //         arrayOptions[i].firstElementChild.classList.add('active');
+            //     } else {
+            //         arrayOptions[i].firstElementChild.classList.remove('active');
 
-                }
-            }
+            //     }
+            // }
         } else {
             entry.target.classList.remove('mostrar-lado');
 
@@ -54,26 +83,41 @@ const animadoDerecha = (entryes) => {
 }
 
 const animadoIzquierda = (entryes) => {
-        // console.log(entryes);
+    // console.log(entryes);
 
-        entryes.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('mostrar-izquierda');
+    entryes.forEach(entry => {
+        if (entry.isIntersecting) {
+            // console.log('es observado');
 
-            } else {
-                console.log('no es observado');
-                entry.target.classList.remove('mostrar-izquierda');
+            entry.target.classList.add('mostrar-izquierda');
 
-            }
-        });
-    }
-    //Efecto suave hacia la derecha de los elementos al hacer scroll
+        } else {
+            // console.log('no es observado');
+            entry.target.classList.remove('mostrar-izquierda');
+
+        }
+    });
+}
+
+
+
+//SELECCION DE ELEMENTOS A OBSERVAR
+const mainSections = document.querySelector('.scrollspy-example');
+const arraySections = Array.from(mainSections.children);
+
 const itemsAnimadosDerecha = document.querySelectorAll('.animado-2')
 const itemsAnimadosIzquierda = document.querySelectorAll('.animado-3');
+
 //Se crea el observador para los elementos que se animan hacia arriba
+const observerSections = new IntersectionObserver(callbackSections, options);
 const observerAnimadoDerecha = new IntersectionObserver(animadoDerecha);
 const observerAnimadoIzquierda = new IntersectionObserver(animadoIzquierda);
 //Se empiezan a observar.
+
+arraySections.forEach((element, i) => {
+    observerSections.observe(element);
+})
+
 itemsAnimadosDerecha.forEach(element => {
     observerAnimadoDerecha.observe(element);
 })
